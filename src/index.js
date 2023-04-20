4f38553d28421ac5fb37aeb7edb44a7794b7cb0d
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-04-20 16:42:06 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 
+import App from "./App";
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
