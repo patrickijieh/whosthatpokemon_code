67ddeef1d0462f2243df1e1f35b1f2e79049720e
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-04-22 17:04:33 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+app.listen(3000, () => console.log('Server running on port 3000'));
+
+app.use(express.json());
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
