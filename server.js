4e7b9b8dceb2b3a221a139b5125b98eac2ad1552
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-04-24 01:51:19 -0400

    Added proxy

--- server.js
+++ server.js
@@ -5,30 +5,6 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
-var http, options, proxy, url;
-
-http = require("http");
-
-url = require("url");
-
-proxy = url.parse(process.env.QUOTAGUARDSTATIC_URL);
-target  = url.parse(process.env.DATABASE_URL);
-
-options = {
-  hostname: proxy.hostname,
-  port: proxy.port || 80,
-  path: target.href,
-  headers: {
-    "Proxy-Authorization": "Basic " + (new Buffer.from(proxy.auth).toString("base64")),
-    "Host" : target.hostname
-  }
-};
-
-http.get(options, function(res) {
-  res.pipe(process.stdout);
-  return console.log("status code", res.statusCode);
-});
-
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
