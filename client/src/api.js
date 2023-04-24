54b9ce2dcfd0b5e0004bb9974aebbcbb02e6a49f
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-04-23 21:42:04 -0400

    Removing unnecessary files

--- client/src/api.js
+++
@@ -1,8 +0,0 @@
-export const getPokemon = async (number) => {
-  try {
-    let url = `https://pokeapi.co/api/v2/pokemon/${number}`;
-    const response = await fetch(url);
-    const pokemon = await response.json();
-    return pokemon;
-  } catch (err) {}
-};
