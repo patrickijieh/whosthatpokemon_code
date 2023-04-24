f3db5381a921cb2d141e808e0c21d871b5a72002
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-04-23 21:54:57 -0400

    Updating server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'build')));
 
