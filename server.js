dfdac4eca7e6d464badd590b42a2d8a0268ff656
Patrick Ijieh <123503029+patrickijieh@users.noreply.github.com>
2023-04-24 01:52:38 -0400

    Updated proxy

--- server.js
+++ server.js
@@ -5,16 +5,38 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require('path');
 
+var url = require('url');
+var HttpsProxyAgent = require('https-proxy-agent');
+var request = require('request');
+
+var endpoint = process.env.DATABASE_URL;
+var proxy = process.env.QUOTAGUARDSHIELD_URL;
+var agent = new HttpsProxyAgent(proxy);
+var options = {
+  uri: url.parse(testEndpoint),
+  agent
+};
+
+function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log('body: ', body);
+  } else {
+    console.log('error: ', error);
+  }
+}
+
+request(options, callback);
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
   });
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+/*mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to MongoDB'));
+db.once('open', () => console.log('Connected to MongoDB'));*/
 
 app.use(express.json());
 
